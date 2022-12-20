96b8eb5da47dbc5a089ceaa5281007862ac62212
Mariana Martinez Araque <marianadelos.martinez@unet.edu.ve>
2022-12-20 14:59:51 -0400

    test formato mostrar hora
    terminado el test de proptypes

--- test/Relog.test.jsx
+++ test/Relog.test.jsx
@@ -5,12 +5,32 @@ import { Relog } from "../src/Relog";
 
 
 describe('pruebas en <Relog/>', () => { 
+
+    const hour = 10;
+    const minute= 30;
+    const time = 'PM';
+
     
     test('debe hacer match con el snapshot', () => { 
         const {container}=render(<Relog/>);
         expect(container).toMatchSnapshot();
      });
 
+     
+     test('deberia de mostrar la hora enviada con los proptypes', () => { 
+
+        render(<Relog hora={hour} min={minute} tiempo={time}/>);
+        // screen.debug();
+        expect(screen.getByTestId('test-hour').innerHTML).toContain(`${hour}:${minute} ${time}`);
+      });
+
+
+      test('debe de incrementar la hora al hacer clic en el boton hora', () => { 
+        
+
+
+        
+       })
      
 
 
